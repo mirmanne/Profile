b6f9b836de68cc8c9e13dd06d69ae57607080d23
Ani <iliketochacha3@gmail.com>
2020-08-10 23:14:54 -0400

    Updates

---
+++ precache-manifest.faba3f5d6198bff84c55cc330a794fe5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b503fb21e7982274dce271fd108b11",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "9f639ece0ff1c7df0ca4",
+    "url": "/Profile/static/css/main.1f360b49.chunk.css"
+  },
+  {
+    "revision": "b26361216249c628040c",
+    "url": "/Profile/static/js/2.0b14a442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Profile/static/js/2.0b14a442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f639ece0ff1c7df0ca4",
+    "url": "/Profile/static/js/main.ca7c1e3b.chunk.js"
+  },
+  {
+    "revision": "754c845e39247d9a4368",
+    "url": "/Profile/static/js/runtime-main.cbf015c9.js"
+  },
+  {
+    "revision": "500b86f76253c9cb30d2b255d7f76e5d",
+    "url": "/Profile/static/media/AnishManne_Resume.500b86f7.pdf"
+  },
+  {
+    "revision": "ecfd83a2b528811d4a382ff7743e6e08",
+    "url": "/Profile/static/media/buildingN.ecfd83a2.png"
+  },
+  {
+    "revision": "f999b70583e993135845cf8a77581d6b",
+    "url": "/Profile/static/media/buildingS.f999b705.png"
+  },
+  {
+    "revision": "140ce41d92a915c7d74f0f87f8d1da81",
+    "url": "/Profile/static/media/city.140ce41d.jpg"
+  },
+  {
+    "revision": "804df1a22cc6fc44fc3de2b785d8b039",
+    "url": "/Profile/static/media/credit.804df1a2.jpg"
+  },
+  {
+    "revision": "bb73d0bb0cd080454a21c0f269d9dc80",
+    "url": "/Profile/static/media/education.bb73d0bb.jpg"
+  },
+  {
+    "revision": "68d95e27659fed7f86a861c2c7d14c6c",
+    "url": "/Profile/static/media/game.68d95e27.jpg"
+  },
+  {
+    "revision": "7f5fd33d5299b66abc7784146d046ced",
+    "url": "/Profile/static/media/gatech.7f5fd33d.jpg"
+  },
+  {
+    "revision": "fee373a18cde19c5761c93afdc155bb4",
+    "url": "/Profile/static/media/hobbies.fee373a1.jpg"
+  },
+  {
+    "revision": "744b8b3c9aad68cf84d03a9992e43f2c",
+    "url": "/Profile/static/media/instagram.744b8b3c.jpeg"
+  },
+  {
+    "revision": "54c3d1f1a9c92a3f0f66a1194ececb8c",
+    "url": "/Profile/static/media/jacket.54c3d1f1.png"
+  },
+  {
+    "revision": "2730a0995e2ed1635009c3bcd0c4e8a0",
+    "url": "/Profile/static/media/mail.2730a099.jpg"
+  },
+  {
+    "revision": "a53fc1eea7fbe221179da07922d98c88",
+    "url": "/Profile/static/media/northview.a53fc1ee.jpg"
+  },
+  {
+    "revision": "cf428772cacd22dfd4995767aee6a9d8",
+    "url": "/Profile/static/media/profile.cf428772.jpg"
+  },
+  {
+    "revision": "544367cde702ef57d91656cbebb9af99",
+    "url": "/Profile/static/media/projects.544367cd.jpg"
+  },
+  {
+    "revision": "02fd4af44308152c192f37f1128555de",
+    "url": "/Profile/static/media/resume.02fd4af4.jpg"
+  },
+  {
+    "revision": "b392154e2db600f9f839f7720349d2e0",
+    "url": "/Profile/static/media/socials.b392154e.jpg"
+  }
+]);
